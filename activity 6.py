886b210f9630555d6dce513cfefb8e53f909da06
HydrationNeed <carelnugraf@gmail.com>
2022-09-29 18:53:07 +0700

    activty 6

---
+++ activity 6.py
@@ -0,0 +1,8 @@
+px = int(input("Enter point px:"))
+py = int(input("Enter point py:"))
+qx = int(input("Enter point qx:"))
+qy = int(input("Enter point qy:"))
+rx = (qx-px)+qx
+ry = (qy-py)+qy
+r = (rx,ry)
+print("the point r =",r)
