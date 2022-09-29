9ec462514489ba149f2eb65486e20186525221cd
HydrationNeed <carelnugraf@gmail.com>
2022-09-29 18:44:09 +0700

    activity 5

---
+++ acitivity 5.py
@@ -0,0 +1,5 @@
+burger = int(input("Enter burger selling price"))
+soda = int(input("Enter soda selling price"))
+combo_meal= int(input("Enter soda selling price"))
+profit = burger+soda-combo_meal
+print("the fixed price is",profit)
